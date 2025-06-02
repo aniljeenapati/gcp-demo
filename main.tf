42c8e312d60090f9018353fe5fac30b265f73c89
aniljeenapati <jeenapati.anil@gmail.com>
2025-06-02 23:33:39 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,79 @@
+provider "google" {
+  project     = var.project_id
+  region      = var.region
+}
+
+data "google_compute_image" "debian" {
+  family  = var.image_family
+  project = var.image_project
+}
+
+resource "google_compute_instance_template" "default" {
+  name_prefix  = var.instance_template_name
+  machine_type = var.machine_type
+
+  disk {
+    auto_delete  = true
+    boot         = true
+    source_image = data.google_compute_image.debian.self_link
+  }
+
+  network_interface {
+    network = "default"
+    access_config {}
+  }
+
+  metadata_startup_script = <<-EOF
+    #!/bin/bash
+    apt-get update
+    apt-get install -y python3-pip git
+    pip3 install flask
+    cd /home
+    git clone ${var.app_repo_url}
+    cd demo-repo
+    nohup python3 app.py &
+  EOF
+}
+
+resource "google_compute_instance_group_manager" "default" {
+  name               = var.instance_group_name
+  base_instance_name = "flask-app"
+  region             = var.region
+  version {
+    instance_template = google_compute_instance_template.default.id
+  }
+  target_size = 2
+}
+
+resource "google_compute_health_check" "default" {
+  name = "flask-health-check"
+  http_health_check {
+    port         = 5000
+    request_path = "/"
+  }
+}
+
+resource "google_compute_backend_service" "default" {
+  name                  = "flask-backend"
+  health_checks         = [google_compute_health_check.default.id]
+  load_balancing_scheme = "EXTERNAL"
+  backend {
+    group = google_compute_instance_group_manager.default.instance_group
+  }
+}
+
+resource "google_compute_url_map" "default" {
+  name            = "flask-url-map"
+  default_service = google_compute_backend_service.default.id
+}
+
+resource "google_compute_target_http_proxy" "default" {
+  name    = "flask-http-proxy"
+  url_map = google_compute_url_map.default.id
+}
+
+resource "google_compute_global_forwarding_rule" "default" {
+  name       = "flask-forwarding-rule"
+  target     = google_compute_target_http_proxy.default.id
+  port_range = "80"
+}
